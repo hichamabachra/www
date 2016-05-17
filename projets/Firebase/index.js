c4e139a22a12eb084db23c4ea31545304e7a0325
hichamabachra <hichamabachra@hotmail.fr>
2016-05-17 13:31:10 -0400

    Commit Firebase
    Firebase

--- projets/Firebase/index.js
+++ projets/Firebase/index.js
@@ -1,6 +1,22 @@
 //(function(){
    "use strict"
-   
+   var myDataRef = new Firebase('https://t5c8i14bdjr.firebaseio-demo.com/');
+      $('#messageInput').keypress(function (e) {
+        if (e.keyCode == 13) {
+          var name = $('#nameInput').val();
+          var text = $('#messageInput').val();
+          myDataRef.push({name: name, text: text});
+          $('#messageInput').val('');
+        }
+      });
+      myDataRef.on('child_added', function(snapshot) {
+        var message = snapshot.val();
+        displayChatMessage(message.name, message.text);
+      });
+      function displayChatMessage(name, text) {
+        $('<div/>').text(text).prepend($('<em/>').text(name+': ')).appendTo($('#messagesDiv'));
+        $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
+      };
   
 
     
